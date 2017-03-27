413c2fbc3c24c0cb4e39d7b35514001654377626
Héctor Flores <frhectorin@gmail.com>
2017-03-27 15:06:06 -0600

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Examen_Rafa_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Examen_Rafa_2.dir/main.c.o"
+  "Examen_Rafa_2.pdb"
+  "Examen_Rafa_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Examen_Rafa_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Examen_Rafa_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Examen_Rafa_2.
+# This may be replaced when dependencies are built.
