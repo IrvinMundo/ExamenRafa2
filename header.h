7ae3d667528b7ceeb24e5dc1e6a5199ac4241240
Héctor Flores <frhectorin@gmail.com>
2017-03-27 16:54:36 -0600

    Todo

--- header.h
+++ header.h
@@ -1,3 +1,2 @@
-void merge(int numbers[], int left, int mid, int right);
-
+void merge_sort(int numbers[], int left, int right);
 void print_array(int arr[], int len);
--- merge-sort.c
+++ merge-sort.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void merge(int numbers[], int left, int mid, int right);
+
 void print_array(int arr[], int len) {
     int i;
     
