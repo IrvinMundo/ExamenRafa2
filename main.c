86a420ced8aa25f5b8c53de79ccb24217812e0ac
Héctor Flores <frhectorin@gmail.com>
2017-03-27 17:18:56 -0600

    Todo

--- main.c
+++ main.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <wait.h>
+#include <sys/wait.h>
 #include "header.h"
+
+#define MSGSIZE  6
 
 int main(int argc, char* argv[]) {
     int len = 7;
-    int arr[] = {23,12,6,5,4,2,21};
-    pid_t pid;
-    if ((pid = fork())) {
-        /* proceso padre */
-        merge_sort(arr, 0, (len/2) - 1);
-        int status =0;
-        waitpid(pid, &status, 0);
-        waitpid(pid, &status, 0);
-        status = WEXITSTATUS(status);
-        printf("res padre = %d\n", res);
-        printf("total = %d\n", res + status);
-    }
-    else {
+    int arr[] = {23,12,6}; //Padre
+    int arr1[]={5,4,2,21}; //Hijo
+
+    int p[2];
+    pipe(p);
+
+    if (fork() == 0) {
         /* proceso hijo */
-        merge_sort(arr, (len/2) - 1,len-1);
-        printf("res hijo = %d\n", res);
-        exit(res);
+        printf("Termine hijo\n");
+        merge_sort(arr1, 0, len - 1);
+        close(p[0]);
+        close(p[2]);
+        dup2(p[1], 1);
+        write(p[1], arr, MSGSIZE);
+    }else{
+        char inbuf[MSGSIZE];
+        close(p[1]);
+        close(p[2]);
+        int status;
+        wait(&status);
+        dup2(p[0], 0);
+        read(p[0],inbuf,MSGSIZE);
+
+        printf("Termine padre\n");
     }
-    print_array(arr, len);
+
+    /* proceso padre
+    close(p[1]);
+    close(p[2]);
+    merge_sort(arr, 0, len - 1);
+    int d;
+    scanf("%d",&d); */
+
+    //print_array(arr, len);
     return 0;
 }
