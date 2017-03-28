a8972b2cc3bdd03d4e9c11e0538c0b8c86534d00
Héctor Flores <frhectorin@gmail.com>
2017-03-27 23:45:51 -0600

    Punto Rafa

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <sys/wait.h>
 #include "header.h"
 
-#define MSGSIZE  6
+#define MSGSIZE  7
 
 int main(int argc, char* argv[]) {
     int len = 7;
@@ -11,26 +11,33 @@ int main(int argc, char* argv[]) {
     int arr1[]={5,4,2,21}; //Hijo
 
     int p[2];
+
     pipe(p);
 
     if (fork() == 0) {
         /* proceso hijo */
         printf("Termine hijo\n");
         merge_sort(arr1, 0, len - 1);
-        close(p[0]);
         close(p[2]);
         dup2(p[1], 1);
-        write(p[1], arr, MSGSIZE);
+        dup2(p[0],0);
+        print_array(arr1,3);
+        char are[7];
+        read(p[0],are,7);
+        write(p[1],are,7);
     }else{
-        char inbuf[MSGSIZE];
+        char inbuf[4];
         close(p[1]);
         close(p[2]);
         int status;
         wait(&status);
         dup2(p[0], 0);
-        read(p[0],inbuf,MSGSIZE);
+        read(p[0],inbuf,7);
+        merge_sort(arr, 0, len - 1);
+        printf("%s",inbuf);
+        //inbuf es arreglo hijo
+        //arr es arreglo padre
 
-        printf("Termine padre\n");
     }
 
     /* proceso padre
--- merge-sort.c
+++ merge-sort.c
@@ -5,7 +5,7 @@ void merge(int numbers[], int left, int mid, int right);
 
 void print_array(int arr[], int len) {
     int i;
-    
+
     for (i=0; i < len; i++) {
         printf("%d\t",arr[i]);
     }
@@ -15,13 +15,13 @@ void print_array(int arr[], int len) {
 void merge_sort(int numbers[], int left, int right)
 {
     int mid;
-    
+
     if (right > left)
     {
         mid = (right + left) / 2;
         merge_sort(numbers, left, mid);
         merge_sort(numbers, mid+1, right);
-        
+
         merge(numbers, left, mid+1, right);
     }
 }
@@ -30,14 +30,14 @@ void merge(int numbers[], const int left, const int mid, const int right)
 {
     int left1 = left;
     int mid1 = mid;
-    
+
     int left_end, num_elements, tmp_pos;
-    
+
     left_end = mid - 1;
     tmp_pos = 0;
     num_elements = right - left + 1;
     int temp[num_elements];
-    
+
     while ((left1 <= left_end) && (mid1 <= right))
     {
         if (numbers[left1] <= numbers[mid1])
@@ -49,7 +49,7 @@ void merge(int numbers[], const int left, const int mid, const int right)
             temp[tmp_pos++] = numbers[mid1++];
         }
     }
-    
+
     while (left1 <= left_end)
     {
         temp[tmp_pos++] = numbers[left1++];
@@ -58,7 +58,7 @@ void merge(int numbers[], const int left, const int mid, const int right)
     {
         temp[tmp_pos++] = numbers[mid1++];
     }
-    
+
     int i;
     left1 = left;
     for (i=0; i < num_elements; i++, left1++)
@@ -66,5 +66,3 @@ void merge(int numbers[], const int left, const int mid, const int right)
         numbers[left1] = temp[i];
     }
 }
-
-
